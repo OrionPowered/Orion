a2695639e8db5caa315404e3e1112d9f208bae62
Alex Sobiek <38387876+alexsobiek@users.noreply.github.com>
2021-12-18 00:41:46 -0600

    Testing

--- server/src/main/java/pro/prysm/orion/server/Orion.java
+++ server/src/main/java/pro/prysm/orion/server/Orion.java
@@ -1,15 +1,34 @@
 package pro.prysm.orion.server;
 
 import pro.prysm.orion.server.event.EventBus;
+import pro.prysm.orion.server.event.EventHandler;
+import pro.prysm.orion.server.event.events.IncomingPacketEvent;
+import pro.prysm.orion.server.event.events.TestEvent;
+import pro.prysm.orion.server.net.Listener;
+import pro.prysm.orion.server.protocol.incoming.Handshake;
 import pro.prysm.orion.server.util.Logger;
 
-public class Orion {
+public class Orion implements pro.prysm.orion.server.event.Listener {
     private final Logger logger;
     public static final EventBus EVENT_BUS = new EventBus();
+    private final Listener listener;
 
     public Orion() {
         this.logger = new Logger("Orion");
         logger.info("Starting Orion...");
+        listener = new Listener();
+        EVENT_BUS.subscribe(this);
+        EVENT_BUS.post(new TestEvent());
+    }
+
+    @EventHandler
+    public void onTestEvent(TestEvent e) {
+        System.out.println("TEST EVENT");
+    }
+
+    @EventHandler
+    public void onHandshake(IncomingPacketEvent e, Handshake packet) {
+        System.out.println("Handshake packet!");
     }
 
     public Logger getLogger() {
