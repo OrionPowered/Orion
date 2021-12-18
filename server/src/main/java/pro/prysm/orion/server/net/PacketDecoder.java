87c7d3195bcfa6d0835f41b59d16af6145144945
Alex Sobiek <38387876+alexsobiek@users.noreply.github.com>
2021-12-18 00:41:33 -0600

    Added packet decoder

--- server/src/main/java/pro/prysm/orion/server/net/PacketDecoder.java
+++ server/src/main/java/pro/prysm/orion/server/net/PacketDecoder.java
@@ -1,4 +1,34 @@
 package pro.prysm.orion.server.net;
 
-public class PacketDecoder {
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageDecoder;
+import pro.prysm.orion.server.Orion;
+import pro.prysm.orion.server.event.events.IncomingPacketEvent;
+import pro.prysm.orion.server.protocol.PacketWriter;
+import pro.prysm.orion.server.protocol.incoming.Handshake;
+import pro.prysm.orion.server.protocol.incoming.IncomingPacket;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class PacketDecoder extends ByteToMessageDecoder {
+    // This is only a temporary solution, eventually this should be moved to its own class or something
+    HashMap<Integer, Class<? extends IncomingPacket>> incomingPackets = new HashMap<>() {{
+        put(0x00, Handshake.class);
+    }};
+
+    @Override
+    protected void decode(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf, List<Object> list) throws Exception {
+        int id = PacketWriter.readVarInt(byteBuf);
+        if (incomingPackets.containsKey(id)) {
+            Class<? extends IncomingPacket> packetClass = incomingPackets.get(id);
+            IncomingPacket packet = (IncomingPacket) packetClass.getConstructors()[0].newInstance();
+            packet.read(byteBuf);
+            Orion.EVENT_BUS.post(new IncomingPacketEvent(), packet);
+        } else {
+            channelHandlerContext.flush();
+            channelHandlerContext.close();
+        }
+    }
 }
