96934719bb98bcb93170fe0a90ddb16622429465
Alex Sobiek <38387876+alexsobiek@users.noreply.github.com>
2021-12-18 00:39:44 -0600

    Added decoder & timeout

--- server/src/main/java/pro/prysm/orion/server/net/Pipeline.java
+++ server/src/main/java/pro/prysm/orion/server/net/Pipeline.java
@@ -3,13 +3,14 @@ package pro.prysm.orion.server.net;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.timeout.ReadTimeoutHandler;
 
 public class Pipeline extends ChannelInitializer<SocketChannel> {
-
     @Override
     protected void initChannel(SocketChannel socketChannel) {
         ChannelPipeline pipeline = socketChannel.pipeline();
         pipeline.addLast("encoder", new PacketEncoder());
-
+        pipeline.addLast("decoder", new PacketDecoder());
+        pipeline.addLast("timeout", new ReadTimeoutHandler(10));
     }
 }
