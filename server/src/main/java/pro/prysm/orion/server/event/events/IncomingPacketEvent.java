96281cfca771822bb1d9bdf8f930e3f28ec80a13
Alex Sobiek <38387876+alexsobiek@users.noreply.github.com>
2021-12-18 00:41:08 -0600

    Packet Events

---
+++ server/src/main/java/pro/prysm/orion/server/event/events/IncomingPacketEvent.java
@@ -0,0 +1,4 @@
+package pro.prysm.orion.server.event.events;
+
+public class IncomingPacketEvent extends PacketEvent {
+}
--- server/src/main/java/pro/prysm/orion/server/protocol/incoming/IncomingPacket.java
+++ server/src/main/java/pro/prysm/orion/server/protocol/incoming/IncomingPacket.java
@@ -1,6 +1,8 @@
 package pro.prysm.orion.server.protocol.incoming;
 
+import io.netty.buffer.ByteBuf;
 import pro.prysm.orion.server.protocol.Packet;
 
 public abstract class IncomingPacket extends Packet {
+    public abstract void read(ByteBuf buf);
 }
