70a203b604d5b0f5954fedecf4e14b6b1fdb04e9
Alex Sobiek <38387876+alexsobiek@users.noreply.github.com>
2021-12-19 02:21:22 -0600

    Made use of ServerListResponse API

--- server/src/main/java/pro/prysm/orion/server/protocol/outgoing/status/SLPResponse.java
+++ server/src/main/java/pro/prysm/orion/server/protocol/outgoing/status/SLPResponse.java
@@ -2,17 +2,28 @@ package pro.prysm.orion.server.protocol.outgoing.status;
 
 import io.netty.buffer.ByteBuf;
 
+import pro.prysm.orion.api.protocol.ServerListResponse;
 import pro.prysm.orion.server.protocol.outgoing.OutgoingPacket;
 
 public class SLPResponse extends OutgoingPacket {
-    private String response;
+
+    private ServerListResponse response;
+
     public SLPResponse() {
         id = 0x00;
-        response = "{\"version\": {\"name\": \"Orion\", \"protocol\": 757}, \"description\": {\"text\": \"Test Server\"}}";
     }
+
+    public void setResponse(ServerListResponse response) {
+        this.response = response;
+    }
+
+    public ServerListResponse getResponse() {
+        return response;
+    }
+
 
     @Override
     public void write(ByteBuf buf) {
-        writeString(response, buf);
+        writeString(response.toJsonString(), buf);
     }
 }
