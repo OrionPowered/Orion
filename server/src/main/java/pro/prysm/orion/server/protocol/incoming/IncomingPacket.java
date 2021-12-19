c054ed067df9ba0a75d02d87cb45421a3a2f0b38
Alex Sobiek <38387876+alexsobiek@users.noreply.github.com>
2021-12-18 19:59:41 -0600

    Added Connection as parameter

--- server/src/main/java/pro/prysm/orion/server/protocol/incoming/IncomingPacket.java
+++ server/src/main/java/pro/prysm/orion/server/protocol/incoming/IncomingPacket.java
@@ -1,8 +1,13 @@
 package pro.prysm.orion.server.protocol.incoming;
 
 import io.netty.buffer.ByteBuf;
+import pro.prysm.orion.server.net.Connection;
 import pro.prysm.orion.server.protocol.Packet;
 
 public abstract class IncomingPacket extends Packet {
+    protected Connection connection;
+    public IncomingPacket(Connection connection) {
+        this.connection = connection;
+    }
     public abstract void read(ByteBuf buf);
 }
