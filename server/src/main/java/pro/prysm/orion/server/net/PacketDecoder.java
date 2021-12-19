c57fc030cdfc5c033fe13a67beea12c5296adf09
Alex Sobiek <38387876+alexsobiek@users.noreply.github.com>
2021-12-18 20:00:26 -0600

    Better packet management (still temporary)

--- server/src/main/java/pro/prysm/orion/server/net/PacketDecoder.java
+++ server/src/main/java/pro/prysm/orion/server/net/PacketDecoder.java
@@ -3,32 +3,52 @@ package pro.prysm.orion.server.net;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.ByteToMessageDecoder;
+import pro.prysm.orion.api.protocol.PacketState;
 import pro.prysm.orion.server.Orion;
 import pro.prysm.orion.server.event.events.IncomingPacketEvent;
 import pro.prysm.orion.server.protocol.PacketWriter;
-import pro.prysm.orion.server.protocol.incoming.Handshake;
+import pro.prysm.orion.server.protocol.incoming.status.Handshake;
 import pro.prysm.orion.server.protocol.incoming.IncomingPacket;
+import pro.prysm.orion.server.protocol.incoming.status.Ping;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class PacketDecoder extends ByteToMessageDecoder {
-    // This is only a temporary solution, eventually this should be moved to its own class or something
-    HashMap<Integer, Class<? extends IncomingPacket>> incomingPackets = new HashMap<>() {{
+    HashMap<Integer, Class<? extends IncomingPacket>> handshakePackets = new HashMap<>() {{
         put(0x00, Handshake.class);
     }};
 
+    HashMap<Integer, Class<? extends IncomingPacket>> statusPackets = new HashMap<>() {{
+        put(0x00, null);
+        put(0x01, Ping.class);
+    }};
+
+    private final ChannelHandler channelHandler;
+    public PacketDecoder(ChannelHandler channelHandler) {
+        this.channelHandler = channelHandler;
+    }
+
     @Override
-    protected void decode(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf, List<Object> list) throws Exception {
+    protected void decode(ChannelHandlerContext ctx, ByteBuf byteBuf, List<Object> list) throws Exception {
+        Connection connection = channelHandler.getConnection(ctx.channel().remoteAddress());
+        PacketState state = connection.getState();
+
+        Map<Integer, Class<? extends IncomingPacket>> packets = null;
+        switch (state) {
+            case HANDSHAKE -> packets = handshakePackets;
+            case STATUS -> packets = statusPackets;
+        }
         int id = PacketWriter.readVarInt(byteBuf);
-        if (incomingPackets.containsKey(id)) {
-            Class<? extends IncomingPacket> packetClass = incomingPackets.get(id);
-            IncomingPacket packet = (IncomingPacket) packetClass.getConstructors()[0].newInstance();
-            packet.read(byteBuf);
-            Orion.EVENT_BUS.post(new IncomingPacketEvent(), packet);
-        } else {
-            channelHandlerContext.flush();
-            channelHandlerContext.close();
+        if (packets != null && packets.containsKey(id)) {
+            System.out.printf("Packet: %d, State: %s\n", id, state);
+            Class<? extends IncomingPacket> packetClass = packets.get(id);
+            if (packetClass != null) {
+                IncomingPacket packet = (IncomingPacket) packetClass.getConstructors()[0].newInstance(connection);
+                packet.read(byteBuf);
+                Orion.EVENT_BUS.post(new IncomingPacketEvent(), packet);
+            }
         }
     }
 }
