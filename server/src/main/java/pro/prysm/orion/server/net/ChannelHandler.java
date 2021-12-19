bbb7ce9c474b806118b74071f0355d217aca03f9
Alex Sobiek <38387876+alexsobiek@users.noreply.github.com>
2021-12-18 19:58:17 -0600

    Added ChannelHandler

---
+++ server/src/main/java/pro/prysm/orion/server/net/ChannelHandler.java
@@ -0,0 +1,33 @@
+package pro.prysm.orion.server.net;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+import java.net.SocketAddress;
+import java.util.HashMap;
+
+public class ChannelHandler extends ChannelInboundHandlerAdapter {
+
+    private final HashMap<Integer, Connection> connections;
+
+    public ChannelHandler() {
+        connections = new HashMap<>();
+    }
+
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        connections.put(ctx.channel().remoteAddress().hashCode(), new Connection(ctx));
+        System.out.println("Handler added");
+    }
+
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+        connections.remove(ctx.channel().remoteAddress().hashCode());
+        System.out.println("Handler removed");
+    }
+
+    public Connection getConnection(SocketAddress remoteAddress) {
+        return connections.get(remoteAddress.hashCode());
+    }
+
+}
