1ca21ec7688e4c907d2feb0750b4fde11943f81c
Alex Sobiek <38387876+alexsobiek@users.noreply.github.com>
2021-12-21 02:33:51 -0600

    Extend packet

--- api/src/main/java/pro/prysm/orion/api/protocol/incoming/IncomingPacket.java
+++ api/src/main/java/pro/prysm/orion/api/protocol/incoming/IncomingPacket.java
@@ -1,9 +1,11 @@
 package pro.prysm.orion.api.protocol.incoming;
 
+import pro.prysm.orion.api.protocol.Packet;
+
 /**
  * @author 254n_m
  * @since 12/21/21 / 12:56 AM
  * This file was created as a part of Orion
  */
-public interface IncomingPacket {
+public interface IncomingPacket extends Packet  {
 }
--- api/src/main/java/pro/prysm/orion/api/protocol/outgoing/OutgoingPacket.java
+++ api/src/main/java/pro/prysm/orion/api/protocol/outgoing/OutgoingPacket.java
@@ -1,9 +1,11 @@
 package pro.prysm.orion.api.protocol.outgoing;
 
+import pro.prysm.orion.api.protocol.Packet;
+
 /**
  * @author 254n_m
  * @since 12/21/21 / 12:52 AM
  * This file was created as a part of Orion
  */
-public interface OutgoingPacket {
+public interface OutgoingPacket extends Packet {
 }
