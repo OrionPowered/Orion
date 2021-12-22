92f0e968f524406471d6ec2588dea7a431d67cff
Alex Sobiek <38387876+alexsobiek@users.noreply.github.com>
2021-12-21 22:36:19 -0600

    Removed debug println

--- server/src/main/java/pro/prysm/orion/server/protocol/handler/PlayHandler.java
+++ server/src/main/java/pro/prysm/orion/server/protocol/handler/PlayHandler.java
@@ -5,6 +5,5 @@ import pro.prysm.orion.server.net.Connection;
 public class PlayHandler extends ProtocolHandler {
     public PlayHandler(Connection connection) {
         super(connection);
-        System.out.println(connection.getProfile().getUniqueId());
     }
 }
