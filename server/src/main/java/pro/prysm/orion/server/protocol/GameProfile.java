4b2a239f30986b3b768716ce721f30401012511a
Alex Sobiek <38387876+alexsobiek@users.noreply.github.com>
2021-12-21 20:23:11 -0600

    Created GameProfile

---
+++ server/src/main/java/pro/prysm/orion/server/protocol/GameProfile.java
@@ -0,0 +1,33 @@
+package pro.prysm.orion.server.protocol;
+
+import com.google.gson.JsonObject;
+import pro.prysm.orion.api.json.JSONParser;
+
+import java.util.UUID;
+
+public class GameProfile {
+    private String username;
+    private UUID uniqueId;
+
+    public GameProfile(String username, UUID uniqueId) {
+        construct(username, uniqueId);
+    }
+
+    public GameProfile(JsonObject joinServerResponse) {
+        JSONParser parser = new JSONParser(joinServerResponse);
+        construct(parser.getString("name"), parser.getUUID("id"));
+    }
+
+    private void construct(String username, UUID uniqueId) {
+        this.username = username;
+        this.uniqueId = uniqueId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public UUID getUniqueId() {
+        return uniqueId;
+    }
+}
