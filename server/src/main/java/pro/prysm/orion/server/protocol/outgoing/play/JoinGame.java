ebb9868c300dabf332c23242f4e0c94a1a2ec82b
Alex Sobiek <38387876+alexsobiek@users.noreply.github.com>
2021-12-22 16:42:47 -0600

    Added getters

--- server/src/main/java/pro/prysm/orion/server/protocol/outgoing/play/JoinGame.java
+++ server/src/main/java/pro/prysm/orion/server/protocol/outgoing/play/JoinGame.java
@@ -1,6 +1,7 @@
 package pro.prysm.orion.server.protocol.outgoing.play;
 
 import io.netty.buffer.ByteBuf;
+import net.kyori.adventure.nbt.CompoundBinaryTag;
 import pro.prysm.orion.api.protocol.outgoing.OutgoingPacket;
 import pro.prysm.orion.server.GameMode;
 
@@ -15,8 +16,8 @@ public class JoinGame extends pro.prysm.orion.server.protocol.outgoing.OutgoingP
     private GameMode gamemode;
     private GameMode previousGamemode;
     private String[] worlds;
-    private Object dimensionCodec;
-    private Object dimension;
+    private CompoundBinaryTag dimensionCodec;
+    private CompoundBinaryTag dimension;
     private String worldName;
     private long hashedSeed;
     private int maxPlayers;
@@ -31,19 +32,80 @@ public class JoinGame extends pro.prysm.orion.server.protocol.outgoing.OutgoingP
         id = 0x26;
     }
 
+    public void setEntityId(int entityId) {
+        this.entityId = entityId;
+    }
+
+    public void setHardcore(boolean hardcore) {
+        this.hardcore = hardcore;
+    }
+
+    public void setGamemode(GameMode gamemode) {
+        this.gamemode = gamemode;
+    }
+
+    public void setPreviousGamemode(GameMode previousGamemode) {
+        this.previousGamemode = previousGamemode;
+    }
+
+    public void setWorlds(String[] worlds) {
+        this.worlds = worlds;
+    }
+
+    public void setDimensionCodec(CompoundBinaryTag dimensionCodec) {
+        this.dimensionCodec = dimensionCodec;
+    }
+
+    public void setDimension(CompoundBinaryTag dimension) {
+        this.dimension = dimension;
+    }
+
+    public void setWorldName(String worldName) {
+        this.worldName = worldName;
+    }
+
+    public void setHashedSeed(long hashedSeed) {
+        this.hashedSeed = hashedSeed;
+    }
+
+    public void setMaxPlayers(int maxPlayers) {
+        this.maxPlayers = maxPlayers;
+    }
+
+    public void setViewDistance(int viewDistance) {
+        this.viewDistance = viewDistance;
+    }
+
+    public void setSimulationDistance(int simulationDistance) {
+        this.simulationDistance = simulationDistance;
+    }
+
+    public void setReducedDebugInfo(boolean reducedDebugInfo) {
+        this.reducedDebugInfo = reducedDebugInfo;
+    }
+
+    public void setRespawnScreen(boolean respawnScreen) {
+        this.respawnScreen = respawnScreen;
+    }
+
+    public void setDebug(boolean debug) {
+        this.debug = debug;
+    }
+
+    public void setFlat(boolean flat) {
+        this.flat = flat;
+    }
+
     @Override
     public void write(ByteBuf buf) {
         buf.writeInt(entityId);
         buf.writeBoolean(hardcore);
-        buf.writeByte(1);   // Gamemode
-        buf.writeByte(1);   // Previous Gamemode
+        buf.writeByte(gamemode.getId());
+        buf.writeByte(previousGamemode.getId());
         writeVarInt(worlds.length, buf);
-        for (String w : worlds) {
-            writeString(w, buf);
-        }
-        // NBT
-        // dimensionCodec
-        // dimension
+        for (String w : worlds) writeString(w, buf);
+        writeNBT(dimensionCodec, buf);
+        writeNBT(dimension, buf);
         writeString(worldName, buf);
         buf.writeLong(hashedSeed);
         writeVarInt(maxPlayers, buf);
