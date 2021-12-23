55a2d6560640162005fe8fe98c2c921e59a89aaf
Alex Sobiek <38387876+alexsobiek@users.noreply.github.com>
2021-12-23 01:17:27 -0600

    Created PlayerMoveEvent

---
+++ api/src/main/java/pro/prysm/orion/api/event/events/PlayerMoveEvent.java
@@ -0,0 +1,45 @@
+package pro.prysm.orion.api.event.events;
+
+import pro.prysm.orion.api.data.Location;
+import pro.prysm.orion.api.entity.Player;
+import pro.prysm.orion.api.event.Cancellable;
+import pro.prysm.orion.api.event.Event;
+
+public class PlayerMoveEvent extends Event implements Cancellable {
+    private boolean cancelled;
+    private final Player player;
+    private final Location from;
+    private Location to;
+
+    public PlayerMoveEvent(Player player, Location to, Location from) {
+        this.player = player;
+        this.to = to;
+        this.from = from;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public Location getTo() {
+        return to;
+    }
+
+    public void setTo(Location to) {
+        this.to = to;
+    }
+
+    public Location getFrom() {
+        return from;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean cancel) {
+        cancelled = cancel;
+    }
+}
