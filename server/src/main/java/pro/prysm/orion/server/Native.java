77f2c3700a08ce133c0cd85433b31f295e4616a5
Alex Sobiek <38387876+alexsobiek@users.noreply.github.com>
2021-12-23 03:09:24 -0600

    Added native loading

---
+++ server/src/main/java/pro/prysm/orion/server/Native.java
@@ -0,0 +1,30 @@
+package pro.prysm.orion.server;
+
+import pro.prysm.orion.api.exception.ResourceNotFoundException;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+
+public class Native {
+
+    /**
+     * Loads a native library from the resources of this Jar
+     * @param name Library to load
+     * @throws ResourceNotFoundException Library not found in resources
+     * @throws IOException Cannot move library out of Jar
+     */
+    public static void load(String name) throws ResourceNotFoundException, IOException {
+        File libDir = new File("lib");
+        if (!libDir.exists()) libDir.mkdirs();
+        File object = new File("lib", name);
+        // if (!object.exists()) {
+            InputStream is = Orion.class.getClassLoader().getResourceAsStream(name);
+            if (is == null) throw new ResourceNotFoundException("Could not find lib " + name + " in jar");
+            Files.copy(is, object.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        // }
+        System.load(object.getAbsolutePath());
+    }
+}
