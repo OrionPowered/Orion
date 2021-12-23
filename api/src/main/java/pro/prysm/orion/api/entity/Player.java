a4e09f975dcb08d7a1d7c0dd048b15f02f4b555e
Alex Sobiek <38387876+alexsobiek@users.noreply.github.com>
2021-12-23 01:17:16 -0600

    Added getLocation() and getSettings()

--- api/src/main/java/pro/prysm/orion/api/entity/Player.java
+++ api/src/main/java/pro/prysm/orion/api/entity/Player.java
@@ -2,9 +2,14 @@ package pro.prysm.orion.api.entity;
 
 import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.identity.Identity;
+import pro.prysm.orion.api.data.ClientSettings;
+import pro.prysm.orion.api.data.GameProfile;
+import pro.prysm.orion.api.data.Location;
 import pro.prysm.orion.api.net.Connection;
 
 public interface Player extends Identity, Audience {
-    public Connection getConnection();
-    // TODO: public GameProfile getProfile();
+    Connection getConnection();
+    GameProfile getProfile();
+    Location getLocation();
+    ClientSettings getSettings();
 }
