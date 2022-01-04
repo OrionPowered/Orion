6ea4357c43932910ad7aacbeb9cfe40bcfdb00c4
jay <70743392+jaydevelopsshit@users.noreply.github.com>
2022-01-03 19:29:31 -0800

    Add a client chat message packet (no handle implementation yet)

---
+++ api/src/main/java/pro/prysm/orion/api/protocol/incoming/play/ChatMessage.java
@@ -0,0 +1,10 @@
+package pro.prysm.orion.api.protocol.incoming.play;
+
+/**
+ * @author Jay
+ * @since 1/3/22
+ * A basic chat message from the client
+ */
+public interface ChatMessage {
+    String getMessage();
+}
