798988934155dac1cddfc06d620caed89d9a1d93
jay <70743392+jaydevelopsshit@users.noreply.github.com>
2022-01-08 00:11:34 -0800

    Add integer lists to TagUtil

--- server/src/main/java/pro/prysm/orion/server/util/TagUtil.java
+++ server/src/main/java/pro/prysm/orion/server/util/TagUtil.java
@@ -6,6 +6,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TagUtil {
+    public static ListBinaryTag byteList(byte... bytes) {
+        List<BinaryTag> binaryTags = new ArrayList<>();
+        for(byte b : bytes) binaryTags.add(ByteBinaryTag.of(b));
+        return ListBinaryTag.of(BinaryTagTypes.BYTE, binaryTags);
+    }
+
+    public static ListBinaryTag shortList(short... shorts) {
+        List<BinaryTag> binaryTags = new ArrayList<>();
+        for(short s : shorts) binaryTags.add(ShortBinaryTag.of(s));
+        return ListBinaryTag.of(BinaryTagTypes.SHORT, binaryTags);
+    }
+
+    public static ListBinaryTag intList(int... ints) {
+        List<BinaryTag> binaryTags = new ArrayList<>();
+        for(int i : ints) binaryTags.add(IntBinaryTag.of(i));
+        return ListBinaryTag.of(BinaryTagTypes.INT, binaryTags);
+    }
+
+    public static ListBinaryTag longList(long... longs) {
+        List<BinaryTag> binaryTags = new ArrayList<>();
+        for(long l : longs) binaryTags.add(LongBinaryTag.of(l));
+        return ListBinaryTag.of(BinaryTagTypes.LONG, binaryTags);
+    }
+
     public static ListBinaryTag floatList(float... floats) {
         List<BinaryTag> binaryTags = new ArrayList<>();
         for(float f : floats) binaryTags.add(FloatBinaryTag.of(f));
