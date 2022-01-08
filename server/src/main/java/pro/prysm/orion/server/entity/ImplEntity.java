a4afa75a2cddbc5aa4fccb762aa1a6fe76127445
jay <70743392+jaydevelopsshit@users.noreply.github.com>
2022-01-08 11:52:20 -0800

    Fix entity id methods in ImplEntity being non static

--- server/src/main/java/pro/prysm/orion/server/entity/ImplEntity.java
+++ server/src/main/java/pro/prysm/orion/server/entity/ImplEntity.java
@@ -1,24 +1,31 @@
 package pro.prysm.orion.server.entity;
 
-import lombok.Data;
+import lombok.*;
 import pro.prysm.orion.api.data.Location;
 import pro.prysm.orion.api.entity.Entity;
 
-@Data
+@Getter
+@ToString
+@EqualsAndHashCode
 public class ImplEntity implements Entity {
     /** The next entity id to be used */
-    private static int nextEntityId;
+    private static int nextEntityId = 0;
 
     /** The entity id of this entity */
     private int entityId;
     /** The {@link Location} of this entity */
+    @Setter
     private Location location;
+
+    public ImplEntity() {
+        this.entityId = useEntityId();
+    }
 
     /**
      * Peeks the next entity id without using it.
      * @return The next entity id
      */
-    public int peekNextEntityId() {
+    public static int peekNextEntityId() {
         return nextEntityId;
     }
 
@@ -26,7 +33,7 @@ public class ImplEntity implements Entity {
      * Uses an entity id; retrieves the next entity id and increments {@link ImplEntity#nextEntityId} by 1.
      * @return The available entity id
      */
-    public int useEntityId() {
+    public static int useEntityId() {
         nextEntityId++;
         return nextEntityId - 1;
     }
