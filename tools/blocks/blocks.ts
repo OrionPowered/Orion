4027dfc019bdda9dd54583d1fbfa7bb332a91eac
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2022-01-08 09:31:08 +0100

    Improve code again

--- tools/blocks/blocks.ts
+++ tools/blocks/blocks.ts
@@ -1,6 +1,7 @@
 import MCData from 'minecraft-data'
 import fs from 'fs'
 import path from 'path'
+
 const version = '1.18'
 
 console.log(`Generating sources for ${version}...`)
@@ -10,47 +11,34 @@ let staticVars = ''
 let listItems = ''
 
 data.blocksArray.forEach(block => {
-  let staticVar = 'public static final Block '
-  staticVar += block.name.toUpperCase()
-  staticVar += ' = getBlock(' + block.id + ');\n\t'
+    let staticVar = `public static final Block ${block.name.toUpperCase()} = getBlock(${block.id});\n\t`
 
-  let listItem = 'new Block('
-  listItem += block.id + ', '
-  listItem += '"' + block.displayName + '", '
-  listItem += '"' + block.name + '", '
-  listItem += ((block.hardness) ? block.hardness.toString() : '0.0') + ', '
-  listItem += ((block.resistance) ? block.resistance.toString() : '0.0') + ', '
-  listItem += block.minStateId + ', '
-  listItem += block.maxStateId + ', '
-  listItem += block.defaultState + ', '
-  listItem += block.diggable + ', '
-  listItem += block.emitLight + ', '
-  listItem += '"' + block.material + '"'
-  if (block.harvestTools != null) {
-    listItem += ', new int[]{'
-    for (const toolId in block.harvestTools) {
-      listItem += toolId + ', '
+    const harvestTools = ""
+    if (block.harvestTools != null) {
+        let tools = ""
+        for (const toolId in block.harvestTools) {
+            tools += `${toolId}, `
+        }
+        return `, new int[]{${tools}}`
     }
-    if (listItem.endsWith(', ')) listItem = listItem.slice(0, -3)
-    listItem += '}'
-  }
-  listItem += '),\n\t\t'
 
-  listItems += listItem
-  staticVars += staticVar
+    let listItem = `new Block(${block.id}, "${block.displayName}", "${block.name}", ${block.hardness ? block.hardness.toString() : '0.0'}, ${block.resistance ? block.resistance.toString() : '0.0'}, ${block.minStateId}, ${block.maxStateId}, ${block.defaultState}, ${block.diggable}, ${block.emitLight}, "${block.material}", ${harvestTools})\n\t\t`
+
+    listItems += listItem
+    staticVars += staticVar
 })
 
 if (listItems.endsWith(',\n\t\t')) listItems = listItems.slice(0, -4)
 
 fs.readFile(path.join(__dirname, 'BlockClass.template'), (err, buffer) => {
-  if (err != null) throw err
-
-  let data = buffer.toString()
-  data = data.replace('%statics%', staticVars)
-  data = data.replace('%list_items%', listItems)
-
-  fs.writeFile(path.join(__dirname, 'Block.java'), data, (err) => {
     if (err != null) throw err
-    console.log('Successfully generated Block class!')
-  })
+
+    let data = buffer.toString()
+    data = data.replace('%statics%', staticVars)
+    data = data.replace('%list_items%', listItems)
+
+    fs.writeFile(path.join(__dirname, 'Block.java'), data, (err) => {
+        if (err != null) throw err
+        console.log('Successfully generated Block class!')
+    })
 })
