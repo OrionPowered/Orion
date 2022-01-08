20713cc97a99fc86943c6dbcb89dd40809bd6b08
Alex Sobiek <38387876+alexsobiek@users.noreply.github.com>
2022-01-08 01:21:01 -0600

    Removed generate_materials

--- tools/generate_materials.ts
+++
@@ -1,7 +0,0 @@
-import MCData from 'minecraft-data'
-const version = "1.18"
-
-console.log(`Generating sources for ${version}...`);
-const data: MCData.IndexedData = MCData(version);
-
-console.log(data.materials);
