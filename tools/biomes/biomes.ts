a89c12779df0f93e7432da587c7f33b03e8c1bc0
Alex Sobiek <38387876+alexsobiek@users.noreply.github.com>
2022-01-08 01:21:15 -0600

    Biome class generation

---
+++ tools/biomes/biomes.ts
@@ -0,0 +1,47 @@
+import MCData from 'minecraft-data'
+import fs from "fs";
+import path from "path";
+const version = "1.18"
+
+console.log(`Generating sources for ${version}...`);
+const data: MCData.IndexedData = MCData(version);
+
+let staticVars: string = "";
+let listItems: string = "";
+
+data.biomesArray.forEach(biome => {
+    let staticVar: string = "public static final Biome "
+    staticVar += biome.name.toUpperCase();
+    staticVar += " = getBiome(" + biome.id + ");\n\t";
+
+    let listItem: string =  "new Biome(";
+    listItem += biome.id + ", ";
+    listItem += "\"" + biome.name + "\", ";
+    listItem += "\"" + biome.category + "\", ";
+    listItem += biome.temperature + ", ";
+    listItem += "\"" + biome.precipitation + "\", ";
+    listItem += biome.depth + ", "
+    listItem += "\"" + biome.dimension + "\", ";
+    listItem += "\"" + biome.displayName + "\", ";
+    listItem += biome.color + ", ";
+    listItem += biome.rainfall + "),";
+    listItem += "\n\t\t";
+
+    listItems += listItem;
+    staticVars += staticVar;
+})
+
+if (listItems.endsWith(",\n\t\t")) listItems = listItems.slice(0, -4);
+
+fs.readFile(path.join(__dirname, "BiomeClass.template"), (err, buffer) => {
+    if (err) throw err;
+
+    let data: string = buffer.toString();
+    data = data.replace("%statics%", staticVars);
+    data = data.replace("%list_items%", listItems);
+
+    fs.writeFile(path.join(__dirname, "Biome.java"), data, (err) => {
+        if (err) throw err;
+        console.log("Successfully generated Biome class!");
+    });
+});
