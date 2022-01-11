05edf4593310d11cf81a36bca18adca0c68f11b5
Alex Sobiek <38387876+alexsobiek@users.noreply.github.com>
2022-01-11 03:34:48 -0600

    Removed caching for now as it is a huge memory leak

--- server/src/main/java/pro/prysm/orion/server/world/LevelManager.java
+++ server/src/main/java/pro/prysm/orion/server/world/LevelManager.java
@@ -1,11 +1,15 @@
-package pro.prysm.orion.server.data;
+package pro.prysm.orion.server.world;
 
 import com.alexsobiek.anvil.AnvilLib;
 import com.alexsobiek.anvil.Chunk;
 import com.alexsobiek.anvil.Level;
 import com.alexsobiek.anvil.Region;
+import lombok.Getter;
+import lombok.Setter;
 import pro.prysm.orion.api.data.Location;
 import pro.prysm.orion.server.Orion;
+import pro.prysm.orion.server.world.dimension.CraftDimension;
+import pro.prysm.orion.server.world.dimension.DimensionProvider;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -28,19 +32,15 @@ public class LevelManager {
         dimension = new CraftDimension();
     }
 
-    public Level getLevel() {
-        return level;
-    }
-
     public Region getRegion(int x, int z) {
         Orion.getLogger().debug("Getting region for level {} at {}, {}", level.getName(), x, z);
-        int[] pos = new int[]{x, z};
-        if (regions.containsKey(pos)) return regions.get(pos);
-        else {
+        //int[] pos = new int[]{x, z};
+        //if (regions.containsKey(pos)) return regions.get(pos);
+        //else {
             Region region = level.getRegion(x, z);
-            regions.put(pos, region);
+            //regions.put(pos, region);
             return region;
-        }
+        //}
     }
 
     public Chunk getChunk(Location location) {
@@ -50,13 +50,11 @@ public class LevelManager {
     public Chunk getChunk(int x, int z) {
         Orion.getLogger().debug("Getting chunk for level {} at {}, {}", level.getName(), x, z);
         int[] pos = new int[]{x, z};
-        if (chunks.containsKey(pos)) return chunks.get(pos);
-        else {
+        //if (chunks.containsKey(pos)) return chunks.get(pos);
+        //else {
             Chunk chunk = getRegion(x >> 5, z >> 5).getChunk(x, z);
-            chunks.put(pos, chunk);
+            //chunks.put(pos, chunk);
             return chunk;
-        }
+        //}
     }
-
-
 }
