326e5ef075a12298a6c20bba534a4bdd46267c50
Alex Sobiek <38387876+alexsobiek@users.noreply.github.com>
2022-01-11 03:33:57 -0600

    Added addition & subtraction functions

--- api/src/main/java/pro/prysm/orion/api/data/Location.java
+++ api/src/main/java/pro/prysm/orion/api/data/Location.java
@@ -16,6 +16,30 @@ public class Location {
         this(position[0], position[1], position[2], rotation[0], rotation[1], onGround);
     }
 
+    public void addX(int x) {
+        this.x += x;
+    }
+
+    public void addY(int y) {
+        this.y += y;
+    }
+
+    public void addZ(int z) {
+        this.z += z;
+    }
+
+    public void subX(int x) {
+        this.x -= x;
+    }
+
+    public void subY(int x) {
+        this.y -= y;
+    }
+
+    public void subZ(int z) {
+        this.z -= z;
+    }
+
     public void set(double x, double y, double z) {
         this.x = x;
         this.y = y;
