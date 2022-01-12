0a9159756c6d59f8eb138eb6746b94f1d360735b
Alex Sobiek <38387876+alexsobiek@users.noreply.github.com>
2022-01-11 23:33:31 -0600

    Temporary region caching

--- server/src/main/java/pro/prysm/orion/server/world/LevelManager.java
+++ server/src/main/java/pro/prysm/orion/server/world/LevelManager.java
@@ -13,6 +13,7 @@ import pro.prysm.orion.server.world.dimension.DimensionProvider;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 
 @Getter
 public class LevelManager {
@@ -21,40 +22,31 @@ public class LevelManager {
     @Setter
     private DimensionProvider dimension;
 
-    // Both region and chunk map keys are integer arrays with a length of 2 for x and z
-    // TODO: Regions and chunks are added to these maps but never removed
-    private final Map<int[], Region> regions = new HashMap<>(); // Region cache
-    private final Map<int[], Chunk> chunks = new HashMap<>(); // Chunk cache
+    private final Map<int[], Region> regions;
 
     public LevelManager(String world) {
         anvil = new AnvilLib();
         level = anvil.loadLevel(world);
         dimension = new CraftDimension();
+        regions = new HashMap<>();
     }
+
+
 
     public Region getRegion(int x, int z) {
         Orion.getLogger().debug("Getting region for level {} at {}, {}", level.getName(), x, z);
-        //int[] pos = new int[]{x, z};
-        //if (regions.containsKey(pos)) return regions.get(pos);
-        //else {
-            Region region = level.getRegion(x, z);
-            //regions.put(pos, region);
-            return region;
-        //}
-    }
-
-    public Chunk getChunk(Location location) {
-        return getChunk((int) Math.floor(location.getX()) >> 4, (int) Math.floor(location.getZ()) >> 4);
-    }
-
-    public Chunk getChunk(int x, int z) {
-        Orion.getLogger().debug("Getting chunk for level {} at {}, {}", level.getName(), x, z);
         int[] pos = new int[]{x, z};
-        //if (chunks.containsKey(pos)) return chunks.get(pos);
-        //else {
-            Chunk chunk = getRegion(x >> 5, z >> 5).getChunk(x, z);
-            //chunks.put(pos, chunk);
-            return chunk;
-        //}
+        if (regions.containsKey(pos)) {
+            return regions.get(pos);
+        } else {
+            Region region = level.getRegion(x, z);
+            regions.put(pos, region);
+            return region;
+        }
+    }
+
+    public CompletableFuture<Chunk> getChunk(int x, int z) {
+        Orion.getLogger().debug("Getting chunk for level {} at {}, {}", level.getName(), x, z);
+        return getRegion(x >> 5, z >> 5).getChunkAsync(x, z);
     }
 }
