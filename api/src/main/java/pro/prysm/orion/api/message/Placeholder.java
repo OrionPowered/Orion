9d5de5512bfa466df21cd1decee1fe2f5d1386d9
Alex Sobiek <38387876+alexsobiek@users.noreply.github.com>
2022-01-16 00:10:01 -0600

    Created placeholder system

---
+++ api/src/main/java/pro/prysm/orion/api/message/Placeholder.java
@@ -0,0 +1,6 @@
+package pro.prysm.orion.api.message;
+
+import java.util.concurrent.Callable;
+
+public interface Placeholder<T> extends Callable<T> {
+}
---
+++ server/src/main/java/pro/prysm/orion/server/message/PlaceholderService.java
@@ -0,0 +1,55 @@
+package pro.prysm.orion.server.message;
+
+import pro.prysm.orion.api.message.Placeholder;
+import pro.prysm.orion.server.util.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PlaceholderService implements pro.prysm.orion.api.message.PlaceholderService {
+    // Placeholders map should have either a String or a Method that can be invoked
+    private final Map<String, Object> placeholders;
+    private final Pattern pattern;
+
+    public PlaceholderService() {
+        placeholders = new HashMap<>();
+        pattern = Pattern.compile("%(.*?)%");
+    }
+
+    public void register(String key, String value) {
+        placeholders.put(sanitizeKey(key), value);
+    }
+
+    public void register(String key, Placeholder<?> value) {
+        placeholders.put(sanitizeKey(key), value);
+    }
+
+    public String resolve(String message) {
+        Matcher matcher = pattern.matcher(message);
+        while (matcher.find()) {
+            if (placeholders.containsKey(sanitizeKey(matcher.group()))) {
+                Object replacement = placeholders.get(sanitizeKey(matcher.group()));
+                if (replacement instanceof String) {
+                    message = message.replace(matcher.group(), (String) replacement);
+                } else if (replacement instanceof Callable) {
+                    try {
+                        String res = ((Callable<?>) replacement).call().toString();
+                        message = message.replace(matcher.group(), res);
+                    } catch (Exception e) {
+                        ExceptionHandler.error("Failed to invoke placeholder method", e);
+                    }
+                }
+            }
+        }
+        return message;
+    }
+
+    private String sanitizeKey(String key) {
+        if (key.startsWith("%")) key = key.substring(1);
+        if (key.endsWith("%")) key = key.substring(0, key.length() - 1);
+        return key.toLowerCase();
+    }
+}
