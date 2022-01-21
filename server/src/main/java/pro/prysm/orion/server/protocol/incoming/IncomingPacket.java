35bd472924df9948de0967ea1ee43c5aefdace2f
Alex Sobiek <38387876+alexsobiek@users.noreply.github.com>
2022-01-20 18:10:46 -0600

    Added @Getter

--- server/src/main/java/pro/prysm/orion/server/protocol/incoming/IncomingPacket.java
+++ server/src/main/java/pro/prysm/orion/server/protocol/incoming/IncomingPacket.java
@@ -1,9 +1,11 @@
 package pro.prysm.orion.server.protocol.incoming;
 
+import lombok.Getter;
 import pro.prysm.orion.server.net.Connection;
 import pro.prysm.orion.server.net.PacketByteBuf;
 import pro.prysm.orion.server.protocol.Packet;
 
+@Getter
 public abstract class IncomingPacket extends Packet {
     protected Connection connection;
 
