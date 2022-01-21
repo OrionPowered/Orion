cd014e16698311a4ac3cf3f44900b6b410358678
Alex Sobiek <38387876+alexsobiek@users.noreply.github.com>
2022-01-20 18:11:10 -0600

    Read to component instead of string

--- server/src/main/java/pro/prysm/orion/server/protocol/incoming/play/ChatMessageIn.java
+++ server/src/main/java/pro/prysm/orion/server/protocol/incoming/play/ChatMessageIn.java
@@ -1,20 +1,22 @@
 package pro.prysm.orion.server.protocol.incoming.play;
 
 import lombok.Getter;
+import net.kyori.adventure.text.Component;
 import pro.prysm.orion.server.net.Connection;
 import pro.prysm.orion.server.net.PacketByteBuf;
 import pro.prysm.orion.server.protocol.incoming.IncomingPacket;
 
 @Getter
-public class ChatMessage extends IncomingPacket {
-    private String message;
+public class ChatMessageIn extends IncomingPacket {
+    private Component message;
 
-    public ChatMessage(Connection connection) {
+    public ChatMessageIn(Connection connection) {
         super(connection);
     }
 
     @Override
     public void read(PacketByteBuf buf) {
-        message = buf.readString();
+        message = buf.readComponent();
+        connection.getHandler().handle(this);
     }
 }
