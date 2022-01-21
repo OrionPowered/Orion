480301fe64d20d3c031541fd64041348d126b192
Alex Sobiek <38387876+alexsobiek@users.noreply.github.com>
2022-01-20 19:20:15 -0600

    Added action bar test

--- test-plugin/src/main/java/pro/prysm/test/listener/PlayerJoin.java
+++ test-plugin/src/main/java/pro/prysm/test/listener/PlayerJoin.java
@@ -9,6 +9,7 @@ import pro.prysm.orion.api.event.event.PlayerJoinEvent;
 public class PlayerJoin implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
+        event.getPlayer().sendActionBar(Component.text("This is an action bar!"));
         event.getPlayer().sendMessage(Component.text("This server is running OrionTestPlugin").color(TextColor.fromHexString("#2fc1fa")));
     }
 }
