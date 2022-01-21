ac19b4514e1905c0eb0dca8f56ac0a549d0e5461
Alex Sobiek <38387876+alexsobiek@users.noreply.github.com>
2022-01-20 18:24:28 -0600

    Made player final

---
+++ test-plugin/src/main/java/pro/prysm/test/listener/PlayerJoin.java
@@ -0,0 +1,14 @@
+package pro.prysm.test.listener;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextColor;
+import pro.prysm.orion.api.event.EventHandler;
+import pro.prysm.orion.api.event.Listener;
+import pro.prysm.orion.api.event.event.PlayerJoinEvent;
+
+public class PlayerJoin implements Listener {
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        event.getPlayer().sendMessage(Component.text("This server is running OrionTestPlugin").color(TextColor.fromHexString("2fc1fa")));
+    }
+}
