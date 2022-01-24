2d77d8f53a7b61b826b18a99488a64b5bc2772bb
Alex Sobiek <38387876+alexsobiek@users.noreply.github.com>
2022-01-23 23:47:00 -0600

    Add getWorld(), getWorldForPlayer(), getWorldNames()

--- server/src/main/java/pro/prysm/orion/server/world/LevelProvider.java
+++ server/src/main/java/pro/prysm/orion/server/world/LevelProvider.java
@@ -2,8 +2,16 @@ package pro.prysm.orion.server.world;
 
 import pro.prysm.orion.server.world.dimension.DimensionProvider;
 
+import java.util.UUID;
+
 public interface LevelProvider {
     DimensionProvider getDimensionProvider();
 
+    World getWorld(String name);
+
+    World getWorldForPlayer(UUID uuid);
+
     World[] getWorlds();
+
+    String[] getWorldNames();
 }
