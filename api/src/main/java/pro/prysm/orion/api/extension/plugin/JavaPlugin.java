1e9e0544ecd38b7c95485b02c4a89f9dcc9ff5e2
Alex Sobiek <38387876+alexsobiek@users.noreply.github.com>
2022-01-24 22:40:04 -0600

    Removed redundant @Getter

--- api/src/main/java/pro/prysm/orion/api/extension/plugin/JavaPlugin.java
+++ api/src/main/java/pro/prysm/orion/api/extension/plugin/JavaPlugin.java
@@ -1,6 +1,5 @@
 package pro.prysm.orion.api.extension.plugin;
 
-import lombok.Getter;
 import pro.prysm.orion.api.Server;
 import pro.prysm.orion.api.extension.AbstractExtension;
 
@@ -9,7 +8,6 @@ import pro.prysm.orion.api.extension.AbstractExtension;
  * @since 12/20/21 / 9:25 PM
  * This file was created as a part of Orion
  */
-@Getter
 public abstract class JavaPlugin extends AbstractExtension {
     protected Server server;
 }
