28061d17407897e7fb60d6889aceafe275649c42
Alex Sobiek <38387876+alexsobiek@users.noreply.github.com>
2022-02-20 00:11:13 -0600

    Removed reload() method

--- common/src/main/java/pro/prysm/orion/common/protocol/Protocol.java
+++ common/src/main/java/pro/prysm/orion/common/protocol/Protocol.java
@@ -1,6 +1,7 @@
 package pro.prysm.orion.common.protocol;
 
 import lombok.Getter;
+import lombok.Setter;
 import pro.prysm.orion.api.entity.player.Player;
 import pro.prysm.orion.api.protocol.status.ServerListResponse;
 import pro.prysm.orion.common.net.Connection;
@@ -14,30 +15,13 @@ public class Protocol {
     public static final int PROTOCOL_NUMBER = 757; // 1.18.1
     private final PacketRegistry packetRegistry;
     private final CipherSuite cipherSuite;
-    private ServerListResponse slp = new ServerListResponse();
+    private final ServerListResponse slp = new ServerListResponse();
+    @Setter
     private AuthenticationProvider auth;
 
     public Protocol() {
         cipherSuite = new CipherSuite();
         packetRegistry = new PacketRegistry();
-        reload();
-    }
-
-    public void reload() {
-        slp.getVersion().setProtocol(PROTOCOL_NUMBER);
-
-//        slpData.getVersion().setName(server.getName());
-//        slpData.setDescription(server.getMotdComponent());
-//        slpData.getPlayers().setMax(server.getMaxPlayers());
-//
-//        try (InputStream is = getClass().getClassLoader().getResourceAsStream("prysm.png")) {
-//            if (is == null)
-//                return;
-//            slpData.setFavicon(ServerListResponse.generateFavicon(is));
-//        } catch (IOException e) {
-//            ExceptionHandler.error(e);
-//        }
-
     }
 
     public void broadcastPacket(List<Player> players, OutgoingPacket packet) {
