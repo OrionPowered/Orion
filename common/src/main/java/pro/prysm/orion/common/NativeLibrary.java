91424b2c638e690cbe5f41d312200e949423565f
Alex Sobiek <38387876+alexsobiek@users.noreply.github.com>
2022-02-27 23:09:07 -0600

    Created NativeLibrary class

---
+++ common/src/main/java/pro/prysm/orion/common/NativeLibrary.java
@@ -0,0 +1,60 @@
+package pro.prysm.orion.common;
+
+import pro.prysm.orion.api.exception.ResourceNotFoundException;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+public class NativeLibrary {
+    private final String name;
+
+    public NativeLibrary(String name) {
+        this.name = System.mapLibraryName(name);
+    }
+
+    public void load() throws ResourceNotFoundException {
+        File libDir = new File("lib");
+        if (!libDir.exists()) libDir.mkdirs();
+        File lib = new File(libDir, name);
+
+        InputStream is = NativeLibrary.class.getClassLoader().getResourceAsStream("META-INF/native/" + name);
+        if (is == null) throw new ResourceNotFoundException("Could not find lib " + name + " in jar");
+
+        try {
+            if (!lib.exists() || (lib.exists() && !Arrays.equals(getDigest(is), getDigest(new FileInputStream(lib))))) {
+                replaceFromJar(lib, is);
+            }
+        } catch (NoSuchAlgorithmException | IOException e) {
+            OrionExceptionHandler.error(e);
+        }
+
+        System.load(lib.getAbsolutePath());
+    }
+
+    private void replaceFromJar(File outputDir, InputStream is) throws IOException {
+        Files.copy(is, outputDir.toPath(), StandardCopyOption.REPLACE_EXISTING);
+    }
+
+    private byte[] getDigest(InputStream is) throws IOException, NoSuchAlgorithmException {
+        byte[] buffer = new byte[1024];
+        MessageDigest complete = MessageDigest.getInstance("MD5");
+        int numRead = 0;
+
+        while(numRead != -1) {
+            numRead = is.read(buffer);
+            if (numRead > 0) {
+                complete.update(buffer, 0, numRead);
+            }
+        }
+
+        is.close();
+        return complete.digest();
+    }
+}
