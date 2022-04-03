b1dcb2a97d52e2fb540113816588ba93e76212f1
Alex Sobiek <38387876+alexsobiek@users.noreply.github.com>
2022-04-02 22:50:45 -0500

    Created EntityAction Enum

---
+++ api/src/main/java/pro/prysm/orion/api/entity/player/EntityAction.java
@@ -0,0 +1,13 @@
+package pro.prysm.orion.api.entity.player;
+
+public enum EntityAction {
+    START_SNEAKING,
+    STOP_SNEAKING,
+    LEAVE_BED,
+    START_SPRINTING,
+    STOP_SPRINTING,
+    START_JUMP_WITH_HORSE,
+    STOP_JUMP_WITH_HORSE,
+    OPEN_HORSE_INVENTORY,
+    START_FLYING_WITH_ELYTRA
+}
