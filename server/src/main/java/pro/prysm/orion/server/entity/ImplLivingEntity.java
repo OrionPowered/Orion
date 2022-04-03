86193fa63a171640cc74f122d984af065d9e4553
Alex Sobiek <38387876+alexsobiek@users.noreply.github.com>
2022-04-03 17:00:02 -0500

    Added velocity to ImplLivingEntity

--- server/src/main/java/pro/prysm/orion/server/entity/ImplLivingEntity.java
+++ server/src/main/java/pro/prysm/orion/server/entity/ImplLivingEntity.java
@@ -2,7 +2,9 @@ package pro.prysm.orion.server.entity;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
+import pro.prysm.orion.server.math.Velocity;
 import pro.prysm.orion.api.entity.LivingEntity;
 
 import java.util.UUID;
@@ -12,8 +14,16 @@ import java.util.UUID;
 @EqualsAndHashCode(callSuper = true)
 public class ImplLivingEntity extends ImplEntity implements LivingEntity {
     protected float headPitch;
+    @Setter
+    protected Velocity velocity = Velocity.NONE;
+    @Setter
+    protected float fallDistance;
 
     public ImplLivingEntity(int entityId, UUID uuid, int entityTypeId) {
         super(entityId, uuid, entityTypeId);
+    }
+
+    public void setVelocity(float xMot, float yMot, float zMot) {
+        velocity.set(xMot, yMot, zMot);
     }
 }
